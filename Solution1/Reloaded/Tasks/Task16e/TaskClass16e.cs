ca93b61d1c199fd120d96855da3820f93812cf79
aszpu <arkadiusz.szpunar@gmail.com>
2019-09-25 23:25:06 +0200

    TASK16E... full

--- Solution1/Reloaded/Tasks/Task16e/TaskClass16e.cs
+++ Solution1/Reloaded/Tasks/Task16e/TaskClass16e.cs
@@ -12,12 +12,18 @@ namespace Reloaded.Tasks.Task16e
         {
             var vectorList = new List<Vector>
             {
-                new Vector(2,3,4),
-                new Vector(2,2,2)
+                new Vector(2,6,3),
+                new Vector(3,4,5)
             };
 
             var sum = new VectorMath().Sum(vectorList[0], vectorList[1]);
-            
+            var sub = new VectorMath().Sub(vectorList[0], vectorList[1]);
+            var cross = new VectorMath().CrossProduct(vectorList[0], vectorList[1]);
+            var dotProd = new VectorMath().DotProduct(vectorList[0], vectorList[1]);
+            var neg = new VectorMath().Neg(vectorList[0]);
+            var length = new VectorMath().Length(vectorList[0]);
+            var mul = new VectorMath().Mul(vectorList[0], 3);
+
             Console.ReadKey();
         }
     }
--- Solution1/Reloaded/Tasks/Task16e/VectorMath.cs
+++ Solution1/Reloaded/Tasks/Task16e/VectorMath.cs
@@ -8,9 +8,6 @@ namespace Reloaded.Tasks.Task16e
 {
     public class VectorMath
     {
-        private double _x;
-        private double _y;
-        private double _z;
 
         public VectorMath()
         {
@@ -29,5 +26,63 @@ namespace Reloaded.Tasks.Task16e
 
             return sumAB;
         }
+
+        public Vector Sub(Vector a, Vector b)
+        {
+            var subAB = new Vector()
+            {
+                X = a.X - b.X,
+                Y = a.Y - b.Y,
+                Z = a.Z - b.Z
+            };
+
+            return subAB;
+        }
+
+        public Vector CrossProduct(Vector a, Vector b)
+        {
+            var crossAB = new Vector()
+            {
+                X = (a.Y * b.Z) - (a.Z * b.Y),
+                Y = (a.Z * b.X) - (a.X * b.Z),
+                Z = (a.X * b.Y) - (a.Y * b.X)
+            };
+
+            return crossAB;
+        }
+
+        public double DotProduct(Vector a, Vector b)
+        {
+            return (a.X * b.X) + (a.Y * b.Y) + (a.Z * b.Z);
+        }
+
+        public Vector Neg(Vector a)
+        {
+            var negAB = new Vector()
+            {
+                X = -a.X,
+                Y = -a.Y,
+                Z = -a.Z
+            };
+            return negAB;
+        }
+
+        public double Length(Vector a)
+        {
+            return Math.Sqrt(Math.Pow(a.X, 2) + Math.Pow(a.Y, 2) + Math.Pow(a.Z, 2));
+        }
+
+        public Vector Mul(Vector a, double factor)
+        {
+            var mulA = new Vector()
+            {
+                X = a.X * factor,
+                Y = a.Y * factor,
+                Z = a.Z * factor
+            };
+
+            return mulA;
+        }
+
     }
 }
